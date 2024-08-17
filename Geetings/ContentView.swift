407d1eade289a2bbebc86c2c5897644dfcbd9212
Nour <mohamednour25@gmail.com>
2024-08-17 10:55:07 +0300

    03

--- Geetings/ContentView.swift
+++ Geetings/ContentView.swift
@@ -11,45 +11,12 @@ struct ContentView: View {
     var body: some View {
         VStack(alignment: .leading) {
 
-            Text("Hello There!")
-                .fontWeight(.bold)
-                .padding()
-                .foregroundStyle(Color.white)
-                .background(Color.green.opacity(0.4))
-                .cornerRadius(20.0)
-                .shadow(color: .green, radius: 5, x:10, y: 10)
-            
-            Text("Welcome to Swift Programming")
-                .fontWeight(.bold)
-                .padding()
-                .foregroundStyle(Color.white)
-                .background(Color.orange.opacity(0.4))
-                .cornerRadius(20.0)
-                .shadow(color: .white, radius: 5, x:10, y: 10)
-            
-            Text("Are you ready to?")
-                .fontWeight(.bold)
-                .padding()
-                .foregroundStyle(Color.white)
-                .background(Color.yellow.opacity(0.4))
-                .cornerRadius(20.0)
-                .shadow(color: .yellow, radius: 5, x:10, y: 10)
-            
-            Text("Start Exploring")
-                .fontWeight(.bold)
-                .padding()
-                .foregroundStyle(Color.white)
-                .background(Color.red.opacity(0.4))
-                .cornerRadius(20.0)
-                .shadow(color: .red, radius: 5, x:10, y: 10)
-            
-            Text("Boom")
-                .fontWeight(.bold)
-                .padding()
-                .foregroundStyle(Color.white)
-                .background(Color.purple.opacity(0.4))
-                .cornerRadius(20.0)
-                .shadow(color: .purple, radius: 5, x:10, y: 10)
+            TextView(vText: "Welcome to Swift Programming", vColor: .orange)
+            TextView(vText: "Are you ready to?", vColor: .green)
+            TextView(vText: "Start Exploring", vColor: .yellow)
+            TextView(vText: "Hellow", vColor: .red)
+            TextView(vText: "Boom", vColor: .purple)
+
         }
         .padding()
     }
---
+++ Geetings/TextView.swift
@@ -0,0 +1,34 @@
+//
+//  TextView.swift
+//  Geetings
+//
+//  Created by administrator on 17/08/2024.
+//
+
+import SwiftUI
+
+
+
+
+struct TextView: View {
+    
+    let vText: String
+    let vColor: Color
+    
+    var body: some View {
+        Text(vText)
+            .fontWeight(.semibold)
+            .padding()
+            .foregroundStyle(Color.white)
+            .background(vColor.opacity(0.4))
+            .cornerRadius(20.0)
+        .shadow(color: vColor, radius: 5, x:10, y: 10)    }
+}
+
+#Preview {
+    VStack {
+        TextView(vText: "Hello There", vColor: .green)
+        TextView(vText: "Hummes", vColor: .blue)
+        TextView(vText: "piece", vColor: .purple)
+    }
+}
