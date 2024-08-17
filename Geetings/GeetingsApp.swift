1a5c62758c8b51b1787f3879b91087ba0351b523
Nour <mohamednour25@gmail.com>
2024-08-17 10:17:00 +0300

    Initial Commit

---
+++ Geetings/GeetingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeetingsApp.swift
+//  Geetings
+//
+//  Created by administrator on 17/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct GeetingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
