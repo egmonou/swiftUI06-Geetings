30bce214f558c96ed96f7842531ab67b2940e7bb
Nour <mohamednour25@gmail.com>
2024-08-17 17:07:39 +0300

    04

--- Geetings/ContentView.swift
+++ Geetings/ContentView.swift
@@ -7,16 +7,40 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+    
+    let messeages: [DataItemModel] = [
+        .init(vText: "Welcome to Swift Programming", vColor: .orange),
+        .init(vText: "Are you ready to?", vColor: .green),
+        .init(vText: "Start Exploring", vColor: .yellow),
+        .init(vText: "Hellow", vColor: .red),
+        .init(vText: "Boom", vColor: .purple),
+        .init(vText: "I love", vColor: .cyan),
+        .init(vText: "Programming", vColor: .indigo)
+    ]
+    
     var body: some View {
         VStack(alignment: .leading) {
-
-            TextView(vText: "Welcome to Swift Programming", vColor: .orange)
-            TextView(vText: "Are you ready to?", vColor: .green)
-            TextView(vText: "Start Exploring", vColor: .yellow)
-            TextView(vText: "Hellow", vColor: .red)
-            TextView(vText: "Boom", vColor: .purple)
-
+            //Title
+            VStack(alignment: .leading, spacing: 0) {
+                Text("Greeting")
+                    .font(.largeTitle)
+                    .fontWeight(.semibold)
+                Text("Explorring IOS programing")
+                    .font(.headline)
+                    .fontWeight(.thin)
+            }
+            
+            Spacer()
+            
+            ForEach(messeages) { messeage in
+                TextView(vText: messeage.vText, vColor: messeage.vColor)
+            }
+            
+            Spacer()
+            Spacer()
+     
         }
         .padding()
     }
---
+++ Geetings/DataItemModel.swift
@@ -0,0 +1,15 @@
+//
+//  DataItemModel.swift
+//  Geetings
+//
+//  Created by administrator on 17/08/2024.
+//
+
+import Foundation
+import SwiftUI
+
+struct DataItemModel: Identifiable {
+    let id = UUID()
+    let vText: String
+    let vColor: Color
+}
